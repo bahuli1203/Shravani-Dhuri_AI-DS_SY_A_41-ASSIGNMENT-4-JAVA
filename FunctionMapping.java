30f37c7846d4cf1a20df61ecce5c142d240b5338
Shravani Dhuri <shrazzz06@gmail.com>
2026-06-06 11:46:54 +0530

    Create FunctionMapping.java

---
+++ FunctionMapping.java
@@ -0,0 +1,21 @@
+public class Calculator {
+static String getName() {
+return "Shravani";
+}
+static int getAge() {
+return 20;
+}
+static boolean getStatus() {
+return true;
+}
+static char getGrade() {
+return 'A';
+}
+public static void main(String[] args) {
+System.out.println("/name -> " + getName());
+System.out.println("/age -> " + getAge());
+System.out.println("/status -> " + getStatus());
+System.out.println("/grade -> " + getGrade());
+
+}
+}
